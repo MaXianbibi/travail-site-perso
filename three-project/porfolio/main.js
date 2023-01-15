39616cd77e1433cfaabdd81b5647706932c5cf62
Justin Morneau <jmorneau@c3r3p4.42quebec.com>
2023-01-15 08:14:03 -0500

    0.05

--- three-project/porfolio/main.js
+++ three-project/porfolio/main.js
@@ -1,19 +1,39 @@
 import * as THREE from 'three';
 import * as dat from 'dat.gui'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { Raycaster } from 'three';
+
+
 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 const light = new THREE.DirectionalLight(0xFFFFFF, 1)
+const lightBehind = new THREE.DirectionalLight(0xFFFFFF, 1)
 const gui = new dat.GUI();
+const rayCaster = new THREE.Raycaster();
+
+const colors = [];
+
+console.log(rayCaster);
+const mouse = {
+	x: undefined,
+	y: undefined
+}
 const world = {
 	plane: {
 		witdh: 10,
+		height: 10,
+		widthSegments: 10,
+		heightSegments: 10
 	}
 }
 
+
+
 light.position.set(0, 0, 1);
+lightBehind.position.set(0, 0, -1);
 renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
@@ -23,22 +43,37 @@ camera.position.z = 5;
 const planeGeometry = new THREE.PlaneGeometry( 10, 10, 10, 10 );
 const planeMaterial = new THREE.MeshPhongMaterial
 ({
-	color: 0xFF0000,
+	// color: 0xFF0000,
 	side: THREE.DoubleSide,
-	flatShading: 1
+	flatShading: 1,
+	vertexColors: true
 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
-
 gui.add(world.plane, 'witdh', 1, 20).onChange(() => {
-	plane.geometry.dispose();
-	plane.geometry = new THREE.PlaneGeometry(world.plane.witdh, 10, 10, 10);
-	random_z();
+	generatePlane();
 })
+gui.add(world.plane, 'height', 1, 20).onChange(() => {
+	generatePlane();
+})
+gui.add(world.plane, 'widthSegments', 1, 20).onChange(() => {
+	generatePlane();
+})
+gui.add(world.plane, 'heightSegments', 1, 20).onChange(() => {
+	generatePlane();
+})
+gui.add(camera.position, 'z', 1, 50);
 
 scene.add(plane);
 scene.add(light);
+scene.add(lightBehind);
 
+function generatePlane () {
+	plane.geometry.dispose();
+	plane.geometry = new THREE.PlaneGeometry(world.plane.witdh, world.plane.height,
+							world.plane.widthSegments, world.plane.heightSegments);
+	random_z();
+}
 
 
 function random_z () {
@@ -56,7 +91,30 @@ function random_z () {
 function animate() {
 	requestAnimationFrame(animate) 
 	renderer.render(scene, camera);
+
+	rayCaster.setFromCamera(mouse, camera);
+	const intersect = rayCaster.intersectObject(plane);
+
+	// if (intersect.length > 0)
+	// {
+	// 	plane.rotateX(0.05);
+	// 	plane.rotateY(0.05);
+	// }
 	
 }
+
+for (let i = 0; i < plane.geometry.attributes.position.count; i++)
+{
+	colors.push(0,0, 1);	
+}
+plane.geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3));
+
+new OrbitControls(camera, renderer.domElement);
 random_z();
 animate();
+
+addEventListener('mousemove', (event) => {
+	mouse.x = (event.clientX / innerWidth) * 2 - 1;
+	mouse.y = -(event.clientY / innerHeight) * 2 + 1;
+	// console.log(mouse)
+})
